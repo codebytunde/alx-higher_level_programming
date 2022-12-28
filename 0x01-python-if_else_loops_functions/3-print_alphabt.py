14448d8a76ea69202bc627612ed40e489bc69359
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-12-27 16:59:32 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+for a in range(97, 97 + 26):
+    if a == 113 or a == 101:
+        continue
+    print("{:c}".format(a), end='')
