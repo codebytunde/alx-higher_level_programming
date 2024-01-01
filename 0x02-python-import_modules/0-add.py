4693e3043bfa073dd5b4e37c40156fc2411c8fe1
codebytunde <yahayaabubakarbabatunde@gmail.com>
2024-01-01 02:56:27 -0800

    new commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    a = 1
-    b = 2
-
-    from add_0 import add
-
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+a = 1
+b = 2
+add_0 = __import__('add_0').add
+print("{} + {} = {}".format(a, b, add_0(a, b)))
 
